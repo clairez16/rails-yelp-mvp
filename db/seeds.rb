5a129f692f2b54082f8568d59fa75a110b3fe5ed
Claire Ziemendorf <claire.ziemendorf@gmail.com>
2018-08-15 14:39:26 +0200

    no design yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.new(name: "Resto Chinois", address: "12 rue de Tocqueville", category: "chinese").save!
+Restaurant.new(name: "La table d'Henri", address: "9 avenue de la ferme", category: "french").save!
+Restaurant.new(name: "A la bonne franquette", address: "54 rue Saint Maure", category: "french").save!
+Restaurant.new(name: "Fukushima", address: "2 rue Levy", category: "japanese").save!
+Restaurant.new(name: "La Forchetta", address: "89 boulevard Saint-Germain", category: "italian").save!
